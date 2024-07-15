340fa306714adcee24c32520987b405828f07ea1
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-07-15 16:09:48 -0300

    feat: implementado um looping no menu para que seja possivel ver mais de um tipo de impressao de dados

--- javascript/selecionarModoDeImpressao.js
+++ javascript/selecionarModoDeImpressao.js
@@ -4,6 +4,8 @@ import { imprimirListaEmJson } from '../javascript/imprimirListaEmJson.js';
 import PromptSync from 'prompt-sync';
 const prompt = PromptSync();
 
+let validacaoWhile = true;
+
 export function selecionarModoDeImpressao() {
 
     console.log('\n');
@@ -11,23 +13,35 @@ export function selecionarModoDeImpressao() {
     console.log('\n');
     console.log('Digite 1 - Imprimir a lista de alunos');
     console.log('Digite 2 - Imprimir em formato Json');
+    console.log('Digite 3 - Finalizar a aplicação');
     console.log('\n');
 
-    let opcao = prompt('Selecione a opção desejada: ');
 
-    switch (opcao) {
-        case '1':
-            console.log('\n');
-            imprimirListaDeAlunos();
-            break;
+    do {
 
-        case '2':
-            console.log('\n');
-            imprimirListaEmJson();
-            break;
+        let opcao = prompt('Selecione a opção desejada: ');
 
-        default:
-            console.log('Opção inválida! Digite 1 para imprimir a lista de alunos ou 2 para imprimir em format Json.');
-            break;
-    }
+        switch (opcao) {
+            case '1':
+                console.log('\n');
+                imprimirListaDeAlunos();
+                break;
+
+            case '2':
+                console.log('\n');
+                imprimirListaEmJson();
+                break;
+
+            case '3':
+                console.log('Aplicação finalizada!');
+                validacaoWhile = false;
+                break;
+
+            default:
+                console.log('Opção inválida! Digite 1 para imprimir a lista de alunos ou 2 para imprimir em format Json.');
+                break;
+        }
+
+    } while (validacaoWhile);
+
 }
